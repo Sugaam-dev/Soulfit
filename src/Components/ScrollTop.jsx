1ae8cb568d50bd483bc992081f0211a74c1f0516
Sugaam-dev <sugaam.dev@gmail.com>
2024-08-21 19:35:45 +0530

    updated

--- src/Components/ScrollTop.jsx
+++ src/Components/ScrollTop.jsx
@@ -1,19 +1,92 @@
-import React from 'react'
-import { FaArrowAltCircleUp } from "react-icons/fa";
-import '../Styles/scrolltop.css'
-function ScrollTop() {
+import React, { useEffect, useState } from "react";
+import styled from "styled-components";
+import { FaArrowUp } from "react-icons/fa";
 
-    const scrol=()=>{
-        window.scrollTo({top: 0,left: 0, behavior: "smooth"});
+const ScrollTop = () => {
+  const [isVisible, setIsVisible] = useState(false);
+
+  const goToBtn = () => {
+    window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
+  };
+
+  const listenToScroll = () => {
+    let heightToHidden = 250;
+    const winScroll =
+      document.body.scrollTop || document.documentElement.scrollTop;
+
+    if (winScroll > heightToHidden) {
+      setIsVisible(true);
+    } else {
+      setIsVisible(false);
     }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", listenToScroll);
+    return () => window.removeEventListener("scroll", listenToScroll);
+  }, []);
 
   return (
-    <>
-      <div className="scrollbut" onClick={scrol}>
-<FaArrowAltCircleUp  className='si'/>
-      </div>
-    </>
-  )
-}
+    <Wrapper>
+      {isVisible && (
+        <div className="top-btn" onClick={goToBtn}>
+          <FaArrowUp className="top-btn--icon" />
+        </div>
+      )}
+    </Wrapper>
+  );
+};
 
-export default ScrollTop
+const Wrapper = styled.section`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: relative;
+
+  .top-btn {
+    font-size: 2.0rem;
+    width: 3rem;
+    height: 3rem;
+    color: #fff;
+    background-color:blue;
+    box-shadow: ;
+    border-radius: 50%;
+    position: fixed;
+    bottom: 5rem;
+    right: 5rem;
+    z-index: 999;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    cursor: pointer;
+
+    &--icon {
+      animation: gototop 1.2s linear infinite alternate-reverse;
+    }
+
+    @keyframes gototop {
+      0% {
+        transform: translateY(-0.5rem);
+      }
+      100% {
+        transform: translateY(.7rem);
+      }
+    }
+  }
+
+  @media (max-width: 450px) {
+    .top-btn {
+      right: 0;
+      left: 85%;
+    }
+  }
+     @media (450px< width <= 1040px) {
+    .top-btn {
+      right: 0;
+      left: 92%;
+      bottom:20%;
+    }
+  }
+`;
+
+export default ScrollTop;
